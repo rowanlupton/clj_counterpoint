35d5d47e8e0aa61db797ce543c0872ec3a3cb836
glassboro42@gmail.com <rowanlupton@gmail.com>
2016-05-07 03:09:11 -0400

    generate-harmony and generator functions are now fully operational.

--- src/clj_counterpoint/gen.clj
+++ src/clj_counterpoint/gen.clj
@@ -1,20 +1,37 @@
 (ns clj-counterpoint.gen)
 
-(declare get-hist get-contour new-contour new-cu-step new-step apply-step)
+(declare generator get-hist get-contour new-contour new-cu-step new-step apply-step)
 
-(defn generate-harmony [melody harmony]
-  (doseq [m melody]
-    (let [hist (get-hist harmony)]
-      (->>
-       hist
-       (get-contour)
-       (new-contour)
-       (new-cu-step)
-       (new-step m)
-       (apply-step harmony))
-      harmony)))
+(defn generate-harmony
+  "takes a melody and returns a harmony"
+  [melody]
+  (let [cnt (count melody)]
+    (take cnt (generator melody))))
 
-#_(def harmony (generate-harmony [67 67 45 56 67 78 77 66 65 66] []))
+#_(generate-harmony [67 67 45 56 67 78 77 66 65 66])
+
+(defn- generator
+  "I need to know:
+    history of the harmony
+    current melody note *for each harmony item
+    harmony vector"
+  ([melody]
+   (generator melody (first melody)))
+  ([melody harmony]
+   #_(println "made it this far")
+   (let [hist (get-hist harmony)]
+     (cons harmony
+           (lazy-seq
+            (->>
+             hist
+             (get-contour)
+             (new-contour)
+             (new-cu-step)
+             (new-step (peek melody))
+             (apply-step harmony)
+             (generator melody)))))))
+
+#_(println (take 4 (generator [75 79 78 70 71 73])))
 
 (defn get-hist
   ""
@@ -28,23 +45,23 @@
 
 (defn new-contour
   ""
-  []
+  [foo]
   ())
 
 (defn new-cu-step
   ""
-  []
+  [foo]
   ())
 
 (defn new-step
   ""
-  []
+  [foo bar]
   ())
 
 (defn apply-step
   ""
-  []
-  ())
+  [foo bar]
+  )
 
 (defn -main []
   ())
